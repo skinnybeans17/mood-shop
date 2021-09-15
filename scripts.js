e252c6f52648d138511afe0e0d23f5e7b70373bf
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-09-15 16:43:15 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,6 +13,24 @@ for (let i = 0; i < data.length; i += 1) {
 	img.height = 300
 	// Add the image to the div
 	newDiv.appendChild(img)
-	console.log(img) // Check the console!
+	// put new div inside items container
 	itemsContainer.appendChild(newDiv)
+	// create a paragraph element for a description
+	const desc = document.createElement('P')
+	// give the paragraph text from the data
+	desc.innerText = data[i].desc
+	// append the paragraph to the div
+	newDiv.appendChild(desc)
+	// do the same thing for price
+	const price = document.createElement('P')
+	price.innerText = data[i].price
+	newDiv.appendChild(price)
+	// Make a button 
+	const button = document.createElement('button')
+	// add an  id name to the button
+	button.id = data[i].name
+	// creates a custom attribute called data-price. That will hold price for each element in the button
+	button.dataset.price = data[i].price
+	button.innerHTML = "Add to Cart"
+	newDiv.appendChild(button)
 }
