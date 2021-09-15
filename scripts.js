c9b6779e5ccdebf36daf829e34386245b9e4a44b
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-09-15 16:34:11 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+import data from './data.js'
+const itemsContainer = document.querySelector('#items')
+// the length of our data determines how many times this loop goes around
+for (let i = 0; i < data.length; i += 1) {
+	// create a new div element and give it a class name
+	const newDiv = document.createElement('div');
+	newDiv.className = 'item'
+	// create an image element
+	const img = document.createElement('img');
+	// this will change each time we go through the loop. Can you explain why?
+	img.src = data[i].image
+	img.width = 300
+	img.height = 300
+	// Add the image to the div
+	newDiv.appendChild(img)
+	console.log(img) // Check the console!
+	itemsContainer.appendChild(newDiv)
+}
